293ce59faf0ba99b8ace3f507d82c0dd57b92db7
shangshenzhen <18839133063@163.com>
2020-12-31 15:05:10 +0800

    自己学习的项目

--- ssz-api/src/main/java/com/ssz/api/servcie/impl/UserServiceImpl.java
+++ ssz-api/src/main/java/com/ssz/api/servcie/impl/UserServiceImpl.java
@@ -25,36 +25,4 @@ public class UserServiceImpl implements UserService {
         productDao.deleteByProductId(dto.getProductId());
         return true;
     }
-
-    public static void main(String[] args) {
-        String str[] = {"f1low","flo","flo1"};
-        Boolean flag = false;
-        int index = 0 ;
-        int minLength = 0;
-        int num = 0;
-        for(int i = 0;i < str.length-1;i++){
-            if (str[i].length()>str[i+1].length()){
-                minLength = str[i+1].length();
-            }else {
-                minLength = str[i].length();
-            }
-        }
-        System.out.println("最小元素长度"+minLength);
-        //拿到最小长度  作为比较次数
-        while (num < minLength){
-            for (int i = 0 ;i < str.length-1;i++){
-                if (str[i].charAt(num) == str[i+1].charAt(num)){
-                    flag = true;
-                }else {
-                    flag = false;
-                    break;
-                }
-            }
-            num++;
-            if (flag){
-                index++;
-            }
-        }
-        System.out.println("相同元素个数:"+index+"相同字符串是:"+str[0].substring(0,index));
-    }
 }
