381cf4659cbcd927ce442af0c063ad312885268e
shangshenzhen <18839133063@163.com>
2020-12-31 17:58:29 +0800

    自己学习的项目

--- ssz-api/src/main/java/com/ssz/api/controller/UserController.java
+++ ssz-api/src/main/java/com/ssz/api/controller/UserController.java
@@ -2,28 +2,22 @@ package com.ssz.api.controller;
 
 import com.ssz.api.servcie.UserService;
 import com.ssz.common.model.dto.UserDTO;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/user")
-@Api(tags = "用户模块")
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping(value = "/insert")
-    @ApiOperation(value = "新增用户接口",notes = "新增用户接口")
-    public Boolean insert(@RequestBody @Valid UserDTO dto){
+    public Boolean insert(@RequestBody  UserDTO dto){
         return userService.insert(dto);
     }
 }
