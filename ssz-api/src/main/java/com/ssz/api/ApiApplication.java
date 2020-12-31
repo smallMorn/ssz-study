7cd21f0bea920c3f5fb2ff153aa54656bdc9678c
shangshenzhen <18839133063@163.com>
2020-12-31 11:38:09 +0800

    自己学习的项目

--- ssz-api/src/main/java/com/ssz/api/ApiApplication.java
+++ ssz-api/src/main/java/com/ssz/api/ApiApplication.java
@@ -3,17 +3,17 @@ package com.ssz.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@SpringBootApplication(scanBasePackages = "com.ssz")
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableFeignClients
 public class ApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
 }
