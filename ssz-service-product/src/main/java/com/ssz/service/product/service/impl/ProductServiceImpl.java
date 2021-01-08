b6c250092492a54e3459f9942c1e7b8fc47c82cc
shangshenzhen <18839133063@163.com>
2021-01-08 17:52:53 +0800

    完成seata集成

--- ssz-service-product/src/main/java/com/ssz/service/product/service/impl/ProductServiceImpl.java
+++ ssz-service-product/src/main/java/com/ssz/service/product/service/impl/ProductServiceImpl.java
@@ -2,9 +2,9 @@ package com.ssz.service.product.service.impl;
 
 import com.ssz.service.product.mapper.ProductMapper;
 import com.ssz.service.product.service.ProductService;
-import io.seata.spring.annotation.GlobalTransactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -14,10 +14,9 @@ public class ProductServiceImpl implements ProductService {
     private final ProductMapper productMapper;
 
     @Override
-    @GlobalTransactional
+    @Transactional
     public Integer deleteById(Long productId) {
         int num = productMapper.deleteByPrimaryKey(productId);
-        int a = 2/0;
         return num;
     }
 }
--- ssz-service-user/src/main/java/com/ssz/service/user/service/impl/UserServiceImpl.java
+++ ssz-service-user/src/main/java/com/ssz/service/user/service/impl/UserServiceImpl.java
@@ -4,22 +4,24 @@ import com.ssz.service.user.entity.User;
 import com.ssz.service.user.mapper.UserMapper;
 import com.ssz.service.user.service.UserService;
 import com.ssz.common.model.dto.UserDTO;
-import io.seata.spring.annotation.GlobalTransactional;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     private final UserMapper userMapper;
 
     @Override
-    @GlobalTransactional
+    @Transactional
     public Boolean insert(UserDTO dto) {
-        System.out.println();
-        userMapper.insert(new User().setUserName(dto.getUserName())
+        int insert = userMapper.insert(new User().setUserName(dto.getUserName())
                 .setUserSex(dto.getUserSex()).setUserAge(dto.getUserAge()));
+        log.info("插入{}", insert == 1 ? "成功" : "失败");
         return true;
     }
 }
