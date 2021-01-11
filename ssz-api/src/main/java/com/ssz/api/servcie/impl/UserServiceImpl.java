bd7c717688ca8142a48057f2c095ebf7f3e95b97
shangshenzhen <18839133063@163.com>
2021-01-11 10:11:53 +0800

    '提交xid'

--- ssz-api/src/main/java/com/ssz/api/servcie/impl/UserServiceImpl.java
+++ ssz-api/src/main/java/com/ssz/api/servcie/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.ssz.api.servcie.UserService;
 import com.ssz.api.dao.ProductDao;
 import com.ssz.common.model.dto.UserDTO;
 import com.ssz.api.dao.UserDao;
+import io.seata.core.context.RootContext;
 import io.seata.spring.annotation.GlobalTransactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +21,7 @@ public class UserServiceImpl implements UserService {
     @Override
     @GlobalTransactional
     public Boolean insert(UserDTO dto) {
+        log.info("xid:{}", RootContext.getXID());
         userDao.insert(dto);
         productDao.deleteByProductId(dto.getProductId());
         return true;
