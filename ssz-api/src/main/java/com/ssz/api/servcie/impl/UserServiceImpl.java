b9d33f912cdd0f3353d32cb1fe2f14ab492a8179
shangshenzhen <18839133063@163.com>
2021-01-11 10:37:36 +0800

    提交忽略文件

--- ssz-api/src/main/java/com/ssz/api/servcie/impl/UserServiceImpl.java
+++ ssz-api/src/main/java/com/ssz/api/servcie/impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ import com.ssz.api.servcie.UserService;
 import com.ssz.api.dao.ProductDao;
 import com.ssz.common.model.dto.UserDTO;
 import com.ssz.api.dao.UserDao;
-import io.seata.core.context.RootContext;
 import io.seata.spring.annotation.GlobalTransactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +20,7 @@ public class UserServiceImpl implements UserService {
     @Override
     @GlobalTransactional
     public Boolean insert(UserDTO dto) {
+        log.info("");
         userDao.insert(dto);
         productDao.deleteByProductId(dto.getProductId());
         return true;
