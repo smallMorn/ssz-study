0c9ae97d318b0e937d0124e04dffc753cdf291e3
shangshenzhen <18839133063@163.com>
2021-03-18 14:59:34 +0800

    去除

--- ssz-api/src/main/java/com/ssz/api/controller/UserController.java
+++ ssz-api/src/main/java/com/ssz/api/controller/UserController.java
@@ -6,8 +6,6 @@ import com.ssz.common.model.dto.UserQueryDTO;
 import com.ssz.common.web.enumerate.ApiCode;
 import com.ssz.common.web.result.ResultInfo;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
@@ -17,8 +15,6 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
-    public static Logger LOG = LoggerFactory.getLogger(UserController.class);
 
     private final UserService userService;
 
