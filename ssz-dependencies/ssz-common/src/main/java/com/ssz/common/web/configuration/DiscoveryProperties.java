62c79374a6e1fa78cdc95b5001a446951c311494
shang.shenzhen@xyb2b.com <12345678>
2022-05-10 19:39:28 +0800

    dd

---
+++ ssz-dependencies/ssz-common/src/main/java/com/ssz/common/web/configuration/DiscoveryProperties.java
@@ -0,0 +1,40 @@
+package com.ssz.common.web.configuration;
+
+import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import com.ssz.common.web.constants.DiscoveryConstant;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+@ConfigurationProperties(DiscoveryConstant.DISCOVERY_PREFIX)
+@Data
+public class DiscoveryProperties {
+
+    public String route = "default";
+
+    public String routeFieldName;
+
+    @Resource
+    private NacosDiscoveryProperties nacosDiscoveryProperties;
+
+    @PostConstruct
+    public void init() {
+        this.overrideFromEnv();
+    }
+
+    private void overrideFromEnv() {
+        if (StringUtils.isEmpty(getRouteFieldName())) {
+            setRouteFieldName(DiscoveryConstant.ROUTE_FIELD_NAME);
+        }
+        String nacosRoute = nacosDiscoveryProperties.getMetadata().get(getRouteFieldName());
+        if (!StringUtils.isEmpty(nacosRoute)) {
+            setRoute(nacosRoute);
+        } else {
+            nacosDiscoveryProperties.getMetadata().put(getRouteFieldName(), getRoute());
+        }
+    }
+
+}
---
+++ ssz-dependencies/ssz-common/src/main/java/com/ssz/common/web/constants/DiscoveryConstant.java
@@ -0,0 +1,11 @@
+package com.ssz.common.web.constants;
+
+public interface DiscoveryConstant {
+
+    String DISCOVERY_PREFIX = "ssz.discovery";
+
+    /**
+     * 默认标签字段命名
+     */
+    String ROUTE_FIELD_NAME = "route";
+}
