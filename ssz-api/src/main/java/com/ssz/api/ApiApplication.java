2d212d25611103005f11f16ea7aaec08b1525aa2
ssz <18839133063@163.com>
2022-06-16 20:31:58 +0800

    优化集成

--- ssz-api/src/main/java/com/ssz/api/ApiApplication.java
+++ ssz-api/src/main/java/com/ssz/api/ApiApplication.java
@@ -3,11 +3,12 @@ package com.ssz.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableFeignClients
 public class ApiApplication {
