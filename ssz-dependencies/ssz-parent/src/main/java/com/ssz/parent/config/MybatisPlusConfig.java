4100d9c45222d6bacb1027a765776f985e45b9b6
ssz <18839133063@163.com>
2022-06-21 20:16:14 +0800

    优化集成

--- ssz-dependencies/ssz-parent/src/main/java/com/ssz/parent/config/MybatisPlusConfig.java
+++ ssz-dependencies/ssz-parent/src/main/java/com/ssz/parent/config/MybatisPlusConfig.java
@@ -1,4 +1,4 @@
-package com.ssz.common.web.config;
+package com.ssz.parent.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.context.annotation.Bean;
