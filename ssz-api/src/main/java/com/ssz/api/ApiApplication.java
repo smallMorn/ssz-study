ddfc962d823f2374ec6e24623599453f8615d4d0
ssz <18839133063@163.com>
2022-06-22 20:50:14 +0800

    优化集成

--- ssz-api/src/main/java/com/ssz/api/ApiApplication.java
+++ ssz-api/src/main/java/com/ssz/api/ApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@SpringBootApplication(scanBasePackages = "com.ssz")
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class ApiApplication {
